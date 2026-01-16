266b0ea3405fe39ab5acd7093bf7f99d59e511a6
huda shafique <hudashafique19@gmail.com>
2026-01-16 22:13:04 +0500

    logic changed

--- pattern2.cpp
+++ pattern2.cpp
@@ -7,8 +7,8 @@ int main() {
     cout <<"enter size : "<<endl;
     cin >> n;
 
-    for (int i = n; i>= 1; i--){
-        for (int j = i; j > 0; j--){
+    for (int i = 1; i <= n; i++){
+        for (int j = n; j >= i; j--){
             cout <<"*";
         }
         cout <<endl;

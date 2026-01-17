e98e1379a8f1b1970a9be03b21040874096f463f
huda shafique <hudashafique19@gmail.com>
2026-01-17 16:34:59 +0500

    logic explained

--- pattern4.cpp
+++ pattern4.cpp
@@ -19,6 +19,14 @@ int main() {
       for (int j = 1; j <= n-i; j++){
         cout <<" ";
       }
+      /*
+      arithmetic sequence logic (2i-1)
+      when i = 1, k = 1
+      i = 2, k = 3
+      i = 4, k = 5
+
+      i = n, k = 2n-1
+      */ 
       for (int k = 1; k <= (2*i)-1; k++){
         cout <<"*";
       }
